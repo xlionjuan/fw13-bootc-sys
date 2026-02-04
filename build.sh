3748102705ebea476386253125074cf6ecd8d4c7
XLion <xlion@xlion.tw>
2026-02-04 23:52:37 +0800

    /opt stuff

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ RELEASE="$(rpm -E %fedora)"
 echo "Creating multiple symlinks that didn't created in the image yet"
 # Create symlink for /opt to /var/opt since it is not created in the image yet
 #mkdir -p "/var/opt" && ln -s "/var/opt"  "/opt"
-mkdir /opt
+mkdir -p /var/opt
 #mkdir -p "/var/usrlocal" && ln -s "/var/usrlocal" "/usr/local"
 
 # https://bootc-dev.github.io/bootc/filesystem.html 
